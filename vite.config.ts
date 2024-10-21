154a6026baa2bbcabf67dedb1f8c9f6ef911fe92
wakashio <wakai1213@gmail.com>
2024-10-21 15:23:36 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/',
+  base: '/ladybug-garden.github.io/',
   server: {
     host: '0.0.0.0'
   }
